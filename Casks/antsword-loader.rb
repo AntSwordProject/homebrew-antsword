f660dee30f481db9aad59be6b8d5089187d77491
Medicean <Medici.Yan@Gmail.com>
2019-06-17 11:19:21 +0800

    upgrade antsword-loader download url

--- Casks/antsword-loader.rb
+++ Casks/antsword-loader.rb
@@ -2,7 +2,7 @@ cask 'antsword-loader' do
   version '4.0.3'
   sha256 'c712f02d2071ca06e4697bf378ede905b9ee14dbd67b1958a0fd071e69e056dd'
 
-  url "https://github.com/AntSwordProject/AntSword-Loader/raw/#{version}/AntSword-Loader-v#{version}-darwin-x64.dmg"
+  url "https://github.com/AntSwordProject/AntSword-Loader/releases/download/#{version}/AntSword-Loader-v#{version}-darwin-x64.dmg"
   appcast 'https://github.com/antswordProject/antsword-Loader/releases.atom'
   name 'AntSword'
   homepage 'https://github.com/AntSwordProject/AntSword/'
@@ -15,4 +15,3 @@ cask 'antsword-loader' do
                '~/Library/Preferences/com.github.AntSwordProject.plist',
              ]
 end
-
